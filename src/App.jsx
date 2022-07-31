0621e5c72738e1fdcd21bf9d6b129e6279c0f467
Mathews A <mathews.mw@gmail.com>
2022-07-31 17:33:48 -0400

    estrutura de aplicação - finalizando sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import { Post } from "./Post"
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import { Sidebar } from './components/Sidebar';
+
+import './global.css';
+import styles from './App.module.css'
 
 function App() {
   return (
     <div>
-      <Post 
-        author="Mathews" 
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae quo consectetur quis a ducimus possimus fuga amet explicabo voluptatibus magni pariatur recusandae, atque voluptatum optio eius asperiores. Numquam, accusantium sit."
-      />
+     
+      <Header />
+      
+      <div className={styles.wrapper}>
+        
+        <Sidebar />
+
+        <main>
+          <Post 
+          author="Mathews" 
+          content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae quo consectetur quis a ducimus possimus fuga amet explicabo voluptatibus magni pariatur recusandae, atque voluptatum optio eius asperiores. Numquam, accusantium sit."
+          />
   
-      <Post
-        author="Carla"
-        content="Um novo post aqui muito legal!" 
-      />
+          <Post
+          author="Carla"
+          content="Um novo post aqui muito legal!" 
+          />
+        </main>
+      </div>
+
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import { PencilSimpleLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return(
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1614850715649-1d0106293bd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=50" alt="" />
+            
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/Mathews-mw.png" alt="" />
+                <strong>Mathews Araujo</strong>
+                <small>Web Dev</small>
+            </div>
+            
+            <footer>
+                <a href="#">
+                    <PencilSimpleLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
